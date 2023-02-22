5990a1b15997446493771247d9174a1e9e56df8b
Maxim <maxim@smspower.org>
2023-02-22 11:36:58 +0000

    Try again

--- encoder/Endian.h
+++ encoder/Endian.h
@@ -5,15 +5,11 @@
 class Endian
 {
     static constexpr uint32_t uint32_ = 0x01020304;
-#ifdef MSVC_VER
-#pragma warning(disable:4310) // Truncating constant
-#endif
-    static constexpr uint8_t magic_ = (const uint8_t&)uint32_;
+    static constexpr uint8_t magic_ = static_cast<const uint8_t&>(uint32_);
 public:
     static constexpr bool little = magic_ == 0x04;
     static constexpr bool middle = magic_ == 0x02;
     static constexpr bool big = magic_ == 0x01;
     static_assert(little || middle || big, "Cannot determine endianness!");
-private:
     Endian() = delete;
 };
